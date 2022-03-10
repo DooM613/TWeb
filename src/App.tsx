c3f197d6ca47825c2ac8c1318342cd94b5169d95
DooM613 <pavel.railian2@mail.ru>
2022-03-10 22:16:17 +0200

    reface the main page of the site

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Railean Pavel CR-192
         </a>
       </header>
     </div>
