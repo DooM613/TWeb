0b1d3f62aff10365ba94b919078505d5d628d16b
DooM613 <pavel.railian2@mail.ru>
2022-04-10 22:15:19 +0300

    Lab4

--- src/Layouts/CustomCard.js
+++ src/Layouts/CustomCard.js
@@ -1,11 +1,13 @@
 import { Card, Col } from 'antd'
 //CustomCard
+
+
 function CustomCard({title, description}) {
 
     return (
 
         <Col span={8}>
-            <div style={{ paddingBottom: '8px',paddingTop:'8px'}}>
+            <div style={{ paddingBottom: '16px',paddingTop:'16px'}}>
                 <Card title={title} bordered={false}>
                     {description}
                 </Card>
