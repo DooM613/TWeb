7650c0113e5e7f653f0675c8412389b36219a767
DooM613 <pavel.railian2@mail.ru>
2022-05-26 18:18:33 +0300

    Lab8

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,51 @@
 import React, {useEffect} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {useLoginStore} from ".";
 import 'antd/dist/antd.css'
+
+import {useLoginStore, useRootStore} from ".";
 import {HeaderLayout} from "./Layouts/HeaderLayout";
 import {Mycontent} from "./Layouts/Mycontent";
 import {Footer} from "antd/es/layout/layout";
 import {Layout} from "antd";
 import {FormLayout} from "./Layouts/FormLayout";
-import {FormLogin} from "./Layouts/FormLogin";
-import {useRootStore} from "./index";
 
+import {FormLogin} from "./Layouts/FormLogin";
+import {Route, Routes} from "react-router-dom";
+import {ROUTES} from "./Routes/Routes";
+import {Home} from "./Layouts/Home";
 function App() {
     const loginS = useLoginStore();
 
     useEffect(() => {
         loginS.setInitialStorageContents()
     }, [])
+
+    const roottoStore = useRootStore()
+    useEffect(() => {
+        roottoStore.setInitialStorageValue()
+        roottoStore.setDatainLocalStorage()
+        roottoStore.getDatafromLocalStorage()
+        // roottoStore.removemyLocalStorage()
+    }, [])
+
     return (
-        <Layout>
-            <HeaderLayout/>
-            <div style={{ padding: '100px 50px 50px 50px'
-            }}>
-                <FormLogin/>
-                <Mycontent />
-            </div>
-            <Footer style={{ textAlign: 'left' }}>Railean Pavel CR-192</Footer>
-        </Layout>
+        <div className="App">
+            <Layout>
+
+                <HeaderLayout/>
+
+                <div className='site-layout-content'>
+
+                    <Routes>
+                        <Route path={ ROUTES[0].path } element={ <Home /> } />
+                        <Route path={ ROUTES[1].path } element={ <Mycontent/> } />
+                        <Route path={ ROUTES[2].path } element={ <FormLogin/> } />
+                    </Routes>
+                </div>
+                <Footer style={{textAlign: 'center'}}>Railean Pavel Cr-192</Footer>
+            </Layout>
+        </div>
     );
 }
 
